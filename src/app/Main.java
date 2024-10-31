2b452164e0d85f2584c862748af4069352d217a0
Nikolaev Igor <nikolaevin91@gmail.com>
2024-10-31 18:07:59 +0200

    Add converter fahrenheit to celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,13 +5,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Converter-Temperature app.");
 
-        double cel = 5;
-        double fahren = convCeliusToFahrenheit(cel);
+        double cls = 5;
+        double frt = 77;
+        double fahren = convCeliusToFahrenheit(cls);
+        double celsius = convFahrenheitToCelius(frt);
         System.out.println("Result is " + fahren + " Fahrenheit.");
+        System.out.println("Result is " + celsius + " Celsius");
 
     }
 
-    private static double convCeliusToFahrenheit(double cel) {
-        return cel / ((double) 5 /9) + 32;
+    private static double convCeliusToFahrenheit(double cls) {
+        return cls / ((double) 5/9) + 32;
+    }
+    private static double convFahrenheitToCelius(double frt){
+        return (frt - 32) * ((double) 5/9);
     }
 }
